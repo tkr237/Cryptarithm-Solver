b375da51ea34868c9a7277864b20f129671603be
tkr237 <trentkrogers@gmail.com>
2016-07-11 19:12:39 -0500

    Fixed Main()

--- SendMoreMoney/Program.cs
+++ SendMoreMoney/Program.cs
@@ -11,19 +11,19 @@ namespace SendMoreMoney
         public static void Main(String[] args)
         {
             #region send + more = money
-            string query = "send + more = money";
-            Solver s = new Solver(query);
-            List<char[]> permutations = s.DeriveAllPermutations(new List<char>(), new List<char> { 's', 'e', 'n', 'd', 'm', 'o', 'r', 'x', 'y', 'z' });
+            //string query = "send + more = money";
+            //Solver s = new Solver(query);
+            //List<char[]> permutations = s.DeriveAllPermutations(new List<char>(), new List<char> { 's', 'e', 'n', 'd', 'm', 'o', 'r', 'x', 'y', 'z' });
             #endregion
             #region logic + logic = prolog
             //string query = "logic + logic = prolog";
-            //SendMoreMoney s = new SendMoreMoney(query);
-            //List<char[]> permutations = s.DeriveAllPermutations(new List<char> { 'l', 'o', 'g', 'i','c','p','r','a','b','d' });
+            //Solver s = new Solver(query);
+            //List<char[]> permutations = s.DeriveAllPermutations(new List<char>(), new List<char> { 'l', 'o', 'g', 'i', 'c', 'p', 'r', 'a', 'b', 'd' });
             #endregion
             #region kyoto + osaka = tokyo
-            //string query = "kyoto + osaka = tokyo";
-            //Solver s = new Solver(query);
-            //List<char[]> permutations = s.DeriveAllPermutations(new List<char> { 'k', 'y', 'o', 't', 's', 'a', 'g', 'h', 'i', 'j' });
+            string query = "kyoto + osaka = tokyo";
+            Solver s = new Solver(query);
+            List<char[]> permutations = s.DeriveAllPermutations(new List<char>(), new List<char> { 'k', 'y', 'o', 't', 's', 'a', 'g', 'h', 'i', 'j' });
             #endregion
 
             //string query = "ab + ba = cc";
